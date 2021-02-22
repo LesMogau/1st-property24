d1dafe3fd1d6318eb0165b30d79738f4abcda0b2
LesMogau <77001661+LesMogau@users.noreply.github.com>
2021-02-22 03:23:14 +0200

    Create authActions.js

---
+++ property24app/src/store/actions/authActions.js
@@ -0,0 +1,45 @@
+export const signIn = (credentials) =>{
+return(dispatch,getState,{ firebase }) =>{
+const firebase = getFirebase();
+
+firebase.auth().signInWithEmailAndPassword(
+    credentials.email,
+    credentials.password
+    ).then(()=>{
+        dispatch({type:'LOGIN_SUCCESS'});
+    }).catch((err)=> {
+        dispatch({type:'LOGIN_ERROR',err});
+    })
+
+}
+}
+
+export const signOut = () => {
+    return (dispatch,getState,{firebase}) =>{
+        const firebase = firebase;
+
+        firebase.auth().signOut().then({
+        dispatch({ type: 'SIGNOUT_SUCCESS' })
+        )}
+    }
+}
+
+export const signUp = (newUser)=>{
+return(dispatch,getState,{ firebase }) =>{
+    const firestore = firebase.firestore();
+    firebase.auth().createUserWithEmailAndPassword(
+        newUser.email,
+        newUser.password
+    ).then((resp) =>{
+return firestore.collection('users').doc(resp.user.uid).set({
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        initials: newUser.firstName[0]+newUser.lastName[0]
+        })
+    }).then(() => {
+        dispatch({ type: 'SIGNUP_SUCCESS' });
+
+    }
+}
+
+} 
